be5a515fe63952a7be7376d09a9cada84dac57ed
Eben <me@ebenroux.co.za>
2024-10-17 17:40:36 +0200

    Implemented unified / async-only / nullable.

--- Shuttle.Core.Uris.Tests/QueryStringFixture.cs
+++ Shuttle.Core.Uris.Tests/QueryStringFixture.cs
@@ -1,49 +1,48 @@
 ﻿using System;
 using NUnit.Framework;
 
-namespace Shuttle.Core.Uris.Tests
+namespace Shuttle.Core.Uris.Tests;
+
+[TestFixture]
+public class QueryStringFixture
 {
-    [TestFixture]
-    public class QueryStringFixture
+    [Test]
+    public void Should_not_be_able_to_add_duplicate_keys()
     {
-        [Test]
-        public void Should_not_be_able_to_add_duplicate_keys()
-        {
-            var qs = new QueryString {{"key", "value"}};
+        var qs = new QueryString { { "key", "value" } };
 
-            Assert.Throws<ArgumentException>(() => qs.Add("key", "value"));
-        }
+        Assert.Throws<ArgumentException>(() => qs.Add("key", "value"));
+    }
 
-        [Test]
-        public void Should_be_able_to_get_from_uri()
-        {
-            var uri = new Uri("the-scheme://the-host?key1=value1&key2=value2");
+    [Test]
+    public void Should_be_able_to_get_from_uri()
+    {
+        var uri = new Uri("the-scheme://the-host?key1=value1&key2=value2");
 
-            var qs = new QueryString(uri);
+        var qs = new QueryString(uri);
 
-            Assert.That(qs.Count, Is.EqualTo(2));
-            Assert.That(qs["key1"], Is.EqualTo("value1"));
-            Assert.That(qs["key2"], Is.EqualTo("value2"));
-        }
+        Assert.That(qs.Count, Is.EqualTo(2));
+        Assert.That(qs["key1"], Is.EqualTo("value1"));
+        Assert.That(qs["key2"], Is.EqualTo("value2"));
+    }
 
-        [Test]
-        public void Should_be_able_to_handle_an_empty_query_string()
-        {
-            var uri = new Uri("the-scheme://the-host");
+    [Test]
+    public void Should_be_able_to_handle_an_empty_query_string()
+    {
+        var uri = new Uri("the-scheme://the-host");
 
-            var qs = new QueryString(uri);
+        var qs = new QueryString(uri);
 
-            Assert.That(qs.Count, Is.EqualTo(0));
-        }
+        Assert.That(qs.Count, Is.EqualTo(0));
+    }
 
-        [Test]
-        public void Should_return_an_empty_string_when_key_not_found()
-        {
-            var uri = new Uri("the-scheme://the-host");
+    [Test]
+    public void Should_return_an_empty_string_when_key_not_found()
+    {
+        var uri = new Uri("the-scheme://the-host");
 
-            var qs = new QueryString(uri);
+        var qs = new QueryString(uri);
 
-            Assert.That(qs["not-here"], Is.Null);
-        }
+        Assert.That(qs["not-here"], Is.Null);
     }
 }
--- Shuttle.Core.Uris/Properties/AssemblyInfo.cs
+++ Shuttle.Core.Uris/Properties/AssemblyInfo.cs
@@ -1,22 +1,11 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NETFRAMEWORK
-[assembly: AssemblyTitle(".NET Framework")]
-#endif
-
-#if NETCOREAPP
-[assembly: AssemblyTitle(".NET Core")]
-#endif
-
-#if NETSTANDARD
-[assembly: AssemblyTitle(".NET Standard")]
-#endif
-
-[assembly: AssemblyVersion("10.0.8.0")]
+[assembly: AssemblyTitle(".NET Unified Platform")]
+[assembly: AssemblyVersion("20.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) 2024, Eben Roux")]
 [assembly: AssemblyProduct("Shuttle.Core.Uris")]
 [assembly: AssemblyCompany("Eben Roux")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.8")]
+[assembly: AssemblyInformationalVersion("20.0.0")]
 [assembly: ComVisible(false)]
